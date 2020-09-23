1d499ef8ebc25c7dd80a9f507789c78be5983773
Towsif824 <tawsif824@gmail.com>
2020-09-23 16:15:56 +0600

    ok

---
+++ FootballClub/FootballClub/Models/FinalDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FootballClub.Models
+{
+    public class FinalDataContext: DbContext
+    {
+        public FinalDataContext()
+        {
+            Database.SetInitializer<FinalDataContext>(new DropCreateDatabaseIfModelChanges<FinalDataContext>());
+        }
+
+        virtual public DbSet<Signup> signups { get; set; }
+        virtual public DbSet<Player> players { get; set; }
+    }
+}
---
+++ FootballClub/FootballClub/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballClub.Models
+{
+    public class Login
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        //ook
+    }
+}
